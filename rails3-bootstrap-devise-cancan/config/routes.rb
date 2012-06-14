e040822669156c3bac9f8f780277f7849c8c58be
Nicholai de Guzman <ngdeguzman@hotmail.com>
2012-06-14 11:14:57 -0500

    added users to database

--- rails3-bootstrap-devise-cancan/config/routes.rb
+++ rails3-bootstrap-devise-cancan/config/routes.rb
@@ -1,4 +1,10 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+
+  root :to => "home#index"
+
   devise_for :users
 
   # The priority is based upon order of creation:
