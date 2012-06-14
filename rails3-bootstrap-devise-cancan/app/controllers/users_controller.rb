6ca30162d696d9adfb64eb8e0775cac37fd79826
Nicholai de Guzman <ngdeguzman@hotmail.com>
2012-06-14 11:42:51 -0500

    Beofre navigation layouts

--- rails3-bootstrap-devise-cancan/app/controllers/users_controller.rb
+++ rails3-bootstrap-devise-cancan/app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 
 class UsersController < ApplicationController
-  def index
-  end
+
+before_filter :authenticate_user!
+  load_and_authorize_resource :only => :index
+
+
+  #def index
+  #  authorize! :index, @user, :message => 'Not authorized as an administrator.'
+  #  @users = User.all
+  #end
 
   def show
+    @user = User.find(params[:id])
   end
+
+
 end
